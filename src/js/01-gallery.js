5cec04a083a23fd21711ad1e851fc9d3b2583591
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-20 13:24:54 +0100

    v1.0.0.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const SimpleLightbox = require('simplelightbox')
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+let backup = ``;
+galleryItems.forEach(item => {
+  backup += `<a class="gallery__item" href=${item.original}>
+    <img class="gallery__image" src=${item.preview} alt=${item.description} />
+  </a>`;
+});
+
+gallery.insertAdjacentHTML('afterbegin', backup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
