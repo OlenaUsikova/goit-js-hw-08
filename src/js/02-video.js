31e479cc4fbfc97713282e8b75227891d7495c27
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-20 14:27:12 +0100

    task2-markup

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player')
+
+const player = new Vimeo.Player(iframe);
+
+ player.on('timeupdate', function(ev) {
+    console.log(ev)
+    });
