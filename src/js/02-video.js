83262e6404397d72866cd3c31b221257adf531b3
OlenaUsikova <121975337+OlenaUsikova@users.noreply.github.com>
2023-03-21 22:15:38 +0100

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,20 @@
 import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 
-const iframe = document.querySelector('#vimeo-player')
+const iframe = document.querySelector('#vimeo-player');
 
 const player = new Player(iframe);
 
- player.on('timeupdate', throttle(function(ev) {
-   localStorage.setItem("videoplayer-current-time", JSON.stringify(ev.seconds)) 
-}, 1000));
+player.on(
+  'timeupdate',
+  throttle(function (ev) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(ev.seconds)
+    );
+  }, 1000)
+);
 
-let playedTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
-console.log(playedTime)
-player.setCurrentTime(playedTime)
+let playedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+console.log(playedTime);
+player.setCurrentTime(playedTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,26 @@
 const throttle = require('lodash.throttle');
 const form = document.querySelector('form');
 
-if(localStorage.getItem("feedback-form-state")){
-    form.elements.email.value = JSON.parse(localStorage.getItem("feedback-form-state")).email || '';
-    form.elements.message.value = JSON.parse(localStorage.getItem("feedback-form-state")).message || '';
+if (localStorage.getItem('feedback-form-state')) {
+  form.elements.email.value =
+    JSON.parse(localStorage.getItem('feedback-form-state')).email || '';
+  form.elements.message.value =
+    JSON.parse(localStorage.getItem('feedback-form-state')).message || '';
 }
 
-function handleInput (ev){
-    ev.preventDefault();
-    let formValues = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
-    formValues[ev.target.name] =  ev.target.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(formValues))
+function handleInput(ev) {
+  ev.preventDefault();
+  let formValues =
+    JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  formValues[ev.target.name] = ev.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formValues));
 }
 
-form.addEventListener('input', throttle(handleInput, 500))
+form.addEventListener('input', throttle(handleInput, 500));
 
-form.addEventListener('submit', (ev) => {
-    ev.preventDefault();
-    console.log(localStorage.getItem("feedback-form-state"));
-    localStorage.clear();
-    form.reset()
-})
+form.addEventListener('submit', ev => {
+  ev.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.clear();
+  form.reset();
+});
